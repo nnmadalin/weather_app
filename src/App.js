8c6dc91ce1b58c1974a99b9fe9cc20366846cee5
nnmadalin <neauna.madalin2006@gmail.com>
2023-08-01 00:14:46 +0300

    location cookie display system

--- src/App.js
+++ src/App.js
@@ -1,30 +1,55 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt  } from '@fortawesome/free-solid-svg-icons';
+import Cookies from 'js-cookie';
 import 'typeface-roboto';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
+
+var is_use = false;
 
 function App() {
 
-  const [inputValue, setInputValue] = useState('');
+  const [inputValue, setInputValue] = useState(Cookies.get('city'));
 
   const handleChangeAndSize = (ev) => {
     const target = ev.target;
     target.style.width = '10px';
     target.style.width = `${target.scrollWidth}px`;
 
+    Cookies.set('city', target.value, { expires: 365 })
+
     handleChange(ev);
   };
-
+  const inputRef = useRef(null);
+  
   const handleChange = (ev) => {
     setInputValue(ev.target.value);
   };
+
+  const addValueOnStartup = (value) => {
+    if (inputRef.current) {
+      inputRef.current.value = value;
+      const target = inputRef.current;
+      target.style.width = '10px';
+      target.style.width = `${target.scrollWidth}px`;
+    }
+  };
+
+  useEffect(() => {
+    const cookieValue = Cookies.get('city');
+    if (!cookieValue) {
+      Cookies.set('city', 'Bucuresti', { expires: 365 });
+    }
+
+    addValueOnStartup(cookieValue);
+
+  }, []);
 
   return (
     <>
       <div className='search_card'>
         <FontAwesomeIcon icon={faMapMarkerAlt} />
-        <input className='search' placeholder='...' onChange={handleChangeAndSize}/>
+        <input ref={inputRef} className='search' placeholder='...' onChange={handleChangeAndSize}/>
       </div>
       <div className='weather_card'>
 
