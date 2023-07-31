6a10256b17c61ec46e5f78b3df810130d5ffae6c
nnmadalin <neauna.madalin2006@gmail.com>
2023-08-01 02:09:14 +0300

    new design

--- src/App.js
+++ src/App.js
@@ -1,59 +1,83 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMapMarkerAlt  } from '@fortawesome/free-solid-svg-icons';
+import { faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons';
+import { FaGithub, FaInstagram, FaFacebook, FaCommentAlt} from "react-icons/fa";
 import Cookies from 'js-cookie';
 import 'typeface-roboto';
 import React, { useState, useEffect, useRef } from 'react';
 import './App.css';
 
-var is_use = false;
-
 function App() {
 
-  const [inputValue, setInputValue] = useState(Cookies.get('city'));
-
-  const handleChangeAndSize = (ev) => {
-    const target = ev.target;
-    target.style.width = '10px';
-    target.style.width = `${target.scrollWidth}px`;
-
-    Cookies.set('city', target.value, { expires: 365 })
-
-    handleChange(ev);
-  };
-  const inputRef = useRef(null);
-  
-  const handleChange = (ev) => {
-    setInputValue(ev.target.value);
-  };
-
-  const addValueOnStartup = (value) => {
-    if (inputRef.current) {
-      inputRef.current.value = value;
-      const target = inputRef.current;
-      target.style.width = '10px';
-      target.style.width = `${target.scrollWidth}px`;
-    }
-  };
+  const [celsiussymbol, setcelsiussymbol] = useState('C');
 
   useEffect(() => {
-    const cookieValue = Cookies.get('city');
+    var cookieValue = Cookies.get('city');
+    var degreesValue = Cookies.get('degrees');
     if (!cookieValue) {
       Cookies.set('city', 'Bucuresti', { expires: 365 });
     }
+    if (!degreesValue) {
+      Cookies.set('degrees', 'celsius', { expires: 365 });
+    }
 
-    addValueOnStartup(cookieValue);
+    cookieValue = Cookies.get('city');
+    degreesValue = Cookies.get('degrees');
+
+    if(degreesValue == "fahrenheit"){
+      setcelsiussymbol('F');
+    }
+    else
+      setcelsiussymbol('C');
 
   }, []);
 
   return (
     <>
-      <div className='search_card'>
-        <FontAwesomeIcon icon={faMapMarkerAlt} />
-        <input ref={inputRef} className='search' placeholder='...' onChange={handleChangeAndSize}/>
-      </div>
-      <div className='weather_card'>
 
+      <div className='container'>
+        <div className='card_small'>
+
+          <div className='top_search'>
+            <FontAwesomeIcon icon={faMapMarkerAlt} size="lg"/>
+            <input value={Cookies.get('city')} placeholder='...'/>
+          </div>
+
+          <div className='top_location'>
+            <h1>undefined</h1>
+          </div>
+
+          <div className='middle_icon'>
+            <img src='icons/question.svg' />
+          </div>
+          
+          <div className='middle_degrees'>
+            <h1 className='degrees'>0</h1>
+            <h1 className='celsius'>°{celsiussymbol}</h1>
+          </div>
+
+          <div className='middle_day'>
+            <h1 className='day'>luni,</h1>
+            <h1 className='hour'>00:00</h1>
+          </div>
+
+          <div className='separator'>
+            <hr />
+          </div>
+
+          <div className='created'>
+            <p>Neauna Madalin</p>
+            <div className='social'>
+              <a href='https://github.com/nnmadalin' target={"_blank"}><FaGithub /></a>
+              <a href='https://www.instagram.com/nnmadalin/' target={"_blank"}><FaInstagram /></a>
+              <a href='https://www.facebook.com/madalin.neauna' target={"_blank"}><FaFacebook /></a>
+            </div>
+          </div>
+        </div>
+        <div className='card_big'>
+          
+        </div>
       </div>
+      
     </>
   );
 }
