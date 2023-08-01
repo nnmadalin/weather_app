5c79f2e6f1c3e763990422fc93c5632314f3bba9
nnmadalin <neauna.madalin2006@gmail.com>
2023-08-01 13:34:57 +0300

    animation scale

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faMapMarkerAlt} from '@fortawesome/free-solid-svg-icons';
-import { FaGithub, FaInstagram, FaFacebook, FaCommentAlt} from "react-icons/fa";
+import { FaGithub, FaInstagram, FaFacebook, FaSearch} from "react-icons/fa";
 import Cookies from 'js-cookie';
 import 'typeface-roboto';
 import React, { useState, useEffect, useRef } from 'react';
@@ -10,8 +10,26 @@ function App() {
 
   const [celsiussymbol, setcelsiussymbol] = useState('C');
   const dayNames = ["Duminică", "Luni", "Marți", "Miercuri", "Joi", "Vineri", "Sâmbătă"];
+  var [date, setDate] = useState(new Date());
+  const [inputValue, setInputValue] = useState(Cookies.get('city'));
+
+  const handleChangeAndSize = (ev) => {
+    const target = ev.target;
+    target.style.width = '10px';
+    target.style.width = `${target.scrollWidth}px`;
+
+    Cookies.set('city', target.value, { expires: 365 })
+
+    handleChange(ev);
+  };
+
+  const handleChange = (ev) => {
+    setInputValue(ev.target.value);
+  };
 
   useEffect(() => {
+
+    //check cookie and create
     var cookieValue = Cookies.get('city');
     var degreesValue = Cookies.get('degrees');
     if (!cookieValue) {
@@ -21,26 +39,34 @@ function App() {
       Cookies.set('degrees', 'celsius', { expires: 365 });
     }
 
+    //get cookie update
     cookieValue = Cookies.get('city');
     degreesValue = Cookies.get('degrees');
 
+    //check if celsius
     if(degreesValue == "fahrenheit"){
       setcelsiussymbol('F');
     }
     else
       setcelsiussymbol('C');
 
+    //refresh page
+    var timer = setInterval(() => setDate(new Date()), 1000);
+      return function cleanup() {
+        clearInterval(timer);
+    };
+
   }, []);
 
   return (
     <>
-
       <div className='container'>
         <div className='card_small'>
 
           <div className='top_search'>
             <FontAwesomeIcon icon={faMapMarkerAlt} size="lg"/>
-            <input value={Cookies.get('city')} placeholder='...'/>
+            <input type = "text" placeholder='Cauta o locatie'/>
+            <button><FaSearch /></button>
           </div>
 
           <div className='top_location'>
@@ -58,7 +84,7 @@ function App() {
 
           <div className='middle_day'>
             <h1 className='day'>{dayNames[(new Date()).getDay()]},</h1>
-            <h1 className='hour'>{(new Date()).getHours() + ":" + (new Date()).getMinutes()}</h1>
+            <h1 className='hour'>{date.getHours() + ":" + date.getMinutes()}</h1>
           </div>
 
           <div className='separator'>
